a6ab2c75f58dc281e44197b74a177d0034665180
Cleon <18450687+meld-cp@users.noreply.github.com>
2022-11-12 17:20:25 +1300

    clean up

--- src/features/feature-inplace-encrypt/DecryptModal.ts
+++ src/features/feature-inplace-encrypt/DecryptModal.ts
@@ -1,48 +1,70 @@
-import { App, Modal } from 'obsidian';
+import { App, Modal, Notice, Setting, TextAreaComponent } from 'obsidian';
 
 export default class DecryptModal extends Modal {
 	text: string;
 	decryptInPlace: boolean = false;
 	showCopyButton: boolean
 
-	constructor(app: App, title: string, text: string = '', showCopyButton:boolean) {
+	constructor(
+		app: App,
+		title: string,
+		text: string = '',
+		showCopyButton:boolean
+	) {
 		super(app);
+		this.titleEl.setText(title);
 		this.text = text;
-		this.titleEl.innerText = title;
 		this.showCopyButton = showCopyButton;
 	}
 
 	onOpen() {
 		let { contentEl } = this;
 
-		const textEl = contentEl.createDiv().createEl('textarea', { text: this.text });
-		textEl.style.width = '100%';
-		textEl.style.height = '100%';
-		textEl.rows = 10;
-		textEl.readOnly = true;
-		//textEl.focus(); // Doesn't seem to work here...
-		setTimeout(() => { textEl.focus() },100); //... but this does
+		let cTextArea : TextAreaComponent;
+		const sText = new Setting(contentEl)
+			.addTextArea( cb=>{
+				cTextArea = cb;
+				cb.setValue(this.text);
+				cb.inputEl.readOnly = true;
+				cb.inputEl.rows = 10;
+				cb.inputEl.style.width = '100%';
+				cb.inputEl.style.resize = 'vertical';
+			})
+		;
+		sText.settingEl.querySelector('.setting-item-info').remove();
 
-
-		const btnContainerEl = contentEl.createDiv('');
+		const sActions =	new Setting(contentEl);
 
 		if (this.showCopyButton){
-			const copyBtnEl = btnContainerEl.createEl('button', { text: 'Copy' });
-			copyBtnEl.addEventListener('click', () => {
-				navigator.clipboard.writeText(textEl.value);
-			});
+
+			sActions
+				.addButton( cb =>{
+					cb
+						.setButtonText('Copy')
+						.onClick( evt =>{
+							navigator.clipboard.writeText( cTextArea.getValue() );
+							new Notice('Copied!');
+						})
+					;
+					if (!this.showCopyButton){
+
+					}
+				})
+			;
 		}
 
-		const decryptInPlaceBtnEl = btnContainerEl.createEl('button', { text: 'Decrypt in-place' });
-		decryptInPlaceBtnEl.addEventListener('click', () => {
-			this.decryptInPlace = true;
-			this.close();
-		});
-
-		const cancelBtnEl = btnContainerEl.createEl('button', { text: 'Close' });
-		cancelBtnEl.addEventListener('click', () => {
-			this.close();
-		});
+		sActions
+			.addButton( cb =>{
+				cb
+					.setWarning()
+					.setButtonText('Decrypt in-place')
+					.onClick( evt =>{
+						this.decryptInPlace = true;
+						this.close();
+					})
+				;
+			})
+		;
 
 	}
 
