4d60259ba61e5d91c385de6906cd718f708d0053
meld-cp <18450687+meld-cp@users.noreply.github.com>
2024-03-16 21:51:09 +1300

    tool - mdenc wip

--- src/tools/mdenc.ts
+++ src/tools/mdenc.ts
@@ -1,4 +1,4 @@
-import yargs, { Options } from 'yargs';
+import * as yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import * as fs from 'fs';
 import * as path from 'path';
@@ -17,56 +17,83 @@ interface Listing {
 class ListCommandHandler {
     
 
-    async argHandler(args: any) {
-        //console.log( "list", args );
-        // for each *.md or *.mdenc file in the current directory, parse it and list any encrypted files
+    async argHandler( format : string ) {
 
-        const listing : Listing[] = [];
+        console.log( 'argHandler', { format } );
 
         const cwd = process.cwd();
     
-        for await (const p of Utils.walk( cwd )) {
+        const listings = await this.fetchListings( cwd );
+
+        this.outputListings( listings, format );
+
+
+    }
+
+    outputListings(listings: Listing[], format: string) {
+
+        if (format == 'csv') {
+            console.log( 'feature,fullPath,relativePath,extension,' );
+            for (const l of listings) {
+                console.log( `"${l.featureType}","${l.fullPath}","${l.relativePath}","${l.extension}"` );
+            }
+            return;
+        }
+        
+        if (format == 'json'){
+            console.log( JSON.stringify( listings, null, 2 ) );
+            return;
+        }
+        
+        console.table( listings );
+
+    }
+
+    async fetchListings( dir : string ) : Promise<Listing[]> {
+        
+        //console.log( 'fetchListings', dir );
+        const listing : Listing[] = [];
+
+        for await (const p of Utils.walk( dir )) {
     
-            const relativePath = '.' + path.sep + path.relative(cwd, p);
+            const relativePath = '.' + path.sep + path.relative(dir, p);
             const ext = path.extname(p).toLowerCase().slice(1);
+
     
-            if ( !['md', ...Constants.ENCRYPTED_FILE_EXTENSIONS, Constants.ENCRYPTED_FILE_EXTENSION_DEFAULT].includes( ext ) ){
+            if ( !['md', ...Constants.ENCRYPTED_FILE_EXTENSIONS].includes( ext ) ){
                 continue;
             }
 
-            const content = await fs.promises.readFile( p, 'utf8' );
-
-            if (
-                ext == 'md'
-                && (
+            
+            if ( ext == 'md' ){
+                const content = await fs.promises.readFile( p, 'utf8' );
+                if (
                     content.includes( InPlaceConstants._PREFIX_A_VISIBLE )
                     || content.includes( InPlaceConstants._PREFIX_B_VISIBLE )
-                )
-            ){
-                listing.push( {
-                    featureType: 'InPlace',
-                    fullPath: p,
-                    relativePath: relativePath,
-                    extension: ext,
-                    //content: content
-                })
-            } else {
-                listing.push( {
-                    featureType: 'WholeNote',
-                    fullPath: p,
-                    relativePath: relativePath,
-                    extension: ext,
-                    //content: content
-                })
+                ){
+                    listing.push( {
+                        featureType: 'InPlace',
+                        fullPath: p,
+                        relativePath: relativePath,
+                        extension: ext,
+                        //content: content
+                    } )
+                }
+                continue;
             }
+            
+            listing.push( {
+                featureType: 'WholeNote',
+                fullPath: p,
+                relativePath: relativePath,
+                extension: ext,
+                //content: content
+            } )
 
         }
-        
-        console.table( listing );
 
-        //console.log( listing.entries( (l) => { relativePath:l.relativePath, l.extension, l.featureType } ) );
+        return listing;
     }
-    
 
 }
 
@@ -80,26 +107,43 @@ class Utils{
     }
 }
 
-const optPasswordList : Options  = {
+const optPasswordList : yargs.Options  = {
     demandOption: true,
     alias: 'pw',
     describe: 'passwords to use',
     type: 'array',
 }
 
+const optListingFormat : yargs.Options = {
+    alias: 'f',
+    describe: 'format of the listing',
+    type: 'string',
+    choices: ['table', 'json', 'csv'],
+    default: 'table',
+    demandOption: true,
+}
+
 const cmdListHandler = new ListCommandHandler();
 
-yargs(hideBin(process.argv))
+yargs.default(hideBin(process.argv))
+    .usage( 'Usage: $0 [command] [options]' )
+    .command( 'list', 'list all encrypted artifacts within the current directory', (yargs) => yargs.option( {
+        format: optListingFormat
+    } ), (argv) => cmdListHandler.argHandler(argv.format as string ) )
     
-    .command( 'list', 'list all encrypted artifacts within the current directory', () => {}, cmdListHandler.argHandler )
-    
-    .command(['test', 'check'], 'check that all notes can be decrypted with the given password list', {
+    .command(['test', 'check'], 'check that all notes can be decrypted with the given password list', (yargs) => yargs.option(  {
         passwords: optPasswordList
-    } )
+    } ) )
     
-    .command('decrypt', 'decrypt notes given a password list and an output directory', {
-        passwords: optPasswordList
-    }, (argv: any) => { console.log( "decrypt", argv ) } )
+    .command('decrypt', 'decrypt notes given a password list and an output directory',  (yargs) => yargs.option(  {
+        passwords: optPasswordList,
+        outdir: {
+            alias: 'o',
+            describe: 'output directory',
+            type: 'string',
+            demandOption: true
+        }
+    } ) )
     
     .demandCommand()
     .help()
@@ -107,6 +151,7 @@ yargs(hideBin(process.argv))
     .example([
         ['$0 list', 'Processes all *.md and *.mdenc files and list any encrypted artifacts within the current directory'],
         ['$0 test --passwords pw1 pw2', 'check that all notes can be decrypted with the given password list'],
+        ['$0 decrypt --passwords pw1 pw2 --outdir out', 'decrypt notes given a password list and an output directory'],
       ])
     .parse()
 ;
